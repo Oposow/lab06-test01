2073992660a48c9c38f677113fca7a614949d289
Dariusz Szymczyk <edarek@op.pl>
2017-05-10 19:03:06 +0200

    Solved.

--- eternal/main.py
+++ eternal/main.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import datetime
 
 
 def calculate(year, month, day):
@@ -10,27 +11,38 @@ def calculate(year, month, day):
     :param day:
     :return:
     """
-    return year + month + day
+
+    try:
+        return datetime.date(year, month, day).weekday()
+    except:
+        return None
 
 
 def main(args):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--year',
-                        type=int,
-                        required=True,
-                        help='Year')
-    parser.add_argument('--month',
-                        type=int,
-                        required=True,
-                        help='Month')
-    parser.add_argument('--day',
-                        type=int,
-                        required=True,
-                        help='Day')
-    parsed_args = parser.parse_args(args)
-    weekday = calculate(parsed_args.year, parsed_args.month, parsed_args.day)
-    print("Weekday {}".format(weekday))
-    return 0
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--year',
+                            type=int,
+                            required=True,
+                            help='Year')
+        parser.add_argument('--month',
+                            type=int,
+                            required=True,
+                            help='Month')
+        parser.add_argument('--day',
+                            type=int,
+                            required=True,
+                            help='Day')
+        parsed_args = parser.parse_args(args)
+        weekday = calculate(parsed_args.year, parsed_args.month, parsed_args.day)
+    except:
+        return 1
+
+    if weekday is None:
+        return 1
+    else:
+        print("Weekday {}".format(weekday))
+        return 0
 
 
 if __name__ == '__main__':
